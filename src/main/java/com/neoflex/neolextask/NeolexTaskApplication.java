9dec4fe6f575e3f9bf18610504b96d02140ae9cc
zufar <zufar.zaynutdinov@ssd.uz>
2023-05-16 17:33:29 +0500

    first commit

---
+++ src/main/java/com/neoflex/neolextask/NeolexTaskApplication.java
@@ -0,0 +1,13 @@
+package com.neoflex.neolextask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeolexTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeolexTaskApplication.class, args);
+    }
+
+}
